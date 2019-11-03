13fbef8b4655c69f3d8f08e1d484c9bee5ea9a32
Damian Saunders <damian@webfoundations.com.au>
2019-11-03 20:57:35 +1100

    Initial webpack config

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require( 'path' );
+
+module.exports = {
+    entry: './src/js/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve( __dirname, './dist' ),
+        publicPath: 'dist/'
+    },
+    mode: 'none',
+    module: {
+        rules: [
+        ]
+    }
+
+};
