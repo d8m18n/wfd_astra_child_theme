9b85fb4f0cf5926f51d27d5dae5e97772dda4583
Damian Saunders <damian@webfoundations.com.au>
2020-01-09 22:28:51 +1100

    Updates to editor colors

--- functions.php
+++ functions.php
@@ -23,7 +23,8 @@ require_once get_theme_file_path( '/includes/editor/editor-setup.php' );
 /**
  * Hooks
  */
-add_action( 'wp_enqueue_scripts', 'wfd_enqueue_styles', 15 );
-add_action( 'after_setup_theme', 'wfd_editor_setup', 10 );
-add_filter( 'astra_color_palettes', 'wfd_astra_color_palettes', 10 );
+add_action( 'wp_enqueue_scripts', 'wfd_enqueue_styles', 10 );
+add_action( 'after_setup_theme', 'wfd_editor_setup', 15 );
+add_action( 'after_setup_theme', 'wfd_editor_gradients', 15 );
+add_filter( 'astra_color_palettes', 'wfd_astra_color_palettes', 15 );
 
