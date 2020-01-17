eff1f8cd5c69a3415d91194ff30ce75c809a0d7d
Damian Saunders <damian@webfoundations.com.au>
2020-01-17 17:20:29 +1100

    Configured blog previous / next navigation

--- functions.php
+++ functions.php
@@ -20,6 +20,7 @@ define( 'WFD_CHILD_DEV_MODE', true );
 require_once get_theme_file_path( '/includes/front/enqueue.php' );
 require_once get_theme_file_path( '/includes/editor/editor-setup.php' );
 require_once get_theme_file_path( '/includes/front/async.php' );
+require_once get_theme_file_path( '/includes/front/blog-navigation.php' );
 
 /**
  * Hooks
@@ -29,4 +30,5 @@ add_action( 'after_setup_theme', 'wfd_editor_setup', 15 );
 add_action( 'after_setup_theme', 'wfd_editor_gradients', 15 );
 add_filter( 'script_loader_tag', 'wfd_filter_script_loader_tag', 10, 2 );
 add_filter( 'astra_color_palettes', 'wfd_astra_color_palettes', 15 );
+add_filter( 'astra_single_post_navigation', 'wfd_change_next_prev_text', 10 );
 
---
+++ includes/front/blog-navigation.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Wfd-child blog post navigation - functions that control how we navigate through posts.
+ *
+ * @package WordPress
+ * @subpackage Wfd-child
+ * @since wfd-child 1.0.0
+ */
+
+/**
+ * Function to change the Next Post/ Previous post text.
+ *
+ * @param array $args Arguments for next post / previous post links.
+ * @return array
+ */
+function wfd_change_next_prev_text( $args ) {
+	$next_post = get_next_post();
+	$prev_post = get_previous_post();
+	$next_text = false;
+	if ( $next_post ) {
+		$next_text = sprintf(
+			'%s <span class="ast-right-arrow">→</span>',
+			$next_post->post_title
+		);
+	}
+	$prev_text = false;
+	if ( $prev_post ) {
+		$prev_text = sprintf(
+			'<span class="ast-left-arrow">←</span> %s',
+			$prev_post->post_title
+		);
+	}
+	$args['next_text'] = $next_text;
+	$args['prev_text'] = $prev_text;
+	return $args;
+}
