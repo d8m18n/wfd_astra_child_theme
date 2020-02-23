df618697921835eca8b7d38108f5528367c931fc
Damian Saunders <damian@webfoundations.com.au>
2020-02-23 19:19:45 +1100

    Added preconnect for google fonts

--- functions.php
+++ functions.php
@@ -22,6 +22,7 @@ require_once get_theme_file_path( '/includes/editor/editor-setup.php' );
 require_once get_theme_file_path( '/includes/front/async-defer.php' );
 require_once get_theme_file_path( '/includes/front/blog-navigation.php' );
 require_once get_theme_file_path( '/includes/front/seofunctions.php' );
+require_once get_theme_file_path( '/includes/front/preconnects.php' );
 
 /**
  * Hooks
@@ -31,6 +32,7 @@ add_action( 'after_setup_theme', 'wfd_editor_setup', 15 );
 add_action( 'after_setup_theme', 'wfd_editor_gradients', 15 );
 add_filter( 'script_loader_tag', 'wfd_filter_script_loader_tag', 10, 2 );
 add_filter( 'script_loader_tag', 'wfd_defer_parsing_of_js', 11, 1 );
+add_filter( 'wp_resource_hints', 'wfd_resource_hints', 10, 2 );
 add_filter( 'astra_color_palettes', 'wfd_astra_color_palettes', 15 );
 add_filter( 'astra_single_post_navigation', 'wfd_change_next_prev_text', 10, 1 );
 add_filter( 'get_avatar', 'wfd_gravatar_alt' );
---
+++ includes/front/preconnects.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Wfd-child preconnect functions. Functions that add preconnects to required resources.
+ *
+ * @package WordPress
+ * @subpackage Wfd-child
+ * @since wfd-child 1.0.0
+ */
+
+/**
+ * Add preconnect for Google Fonts.
+ *
+ * @since Twenty Seventeen 1.0
+ *
+ * @param array  $urls           URLs to print for resource hints.
+ * @param string $relation_type  The relation type the URLs are printed.
+ * @return array $urls           URLs to print for resource hints.
+ */
+function wfd_resource_hints( $urls, $relation_type ) {
+		$urls[] = array(
+			'href' => 'https://fonts.gstatic.com',
+			'crossorigin',
+		);
+	return $urls;
+}
+
+
+
