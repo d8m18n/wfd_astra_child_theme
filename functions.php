5746e36790f8f7e9cbea3b731dca37da1c452c74
Damian Saunders <damian@webfoundations.com.au>
2020-03-20 16:40:13 +1100

    Setup social links menu and integrated icon system from twentynineteen theme.

--- functions.php
+++ functions.php
@@ -22,6 +22,9 @@ require_once get_theme_file_path( '/includes/editor/editor-setup.php' );
 require_once get_theme_file_path( '/includes/front/async-defer.php' );
 require_once get_theme_file_path( '/includes/front/blog-navigation.php' );
 require_once get_theme_file_path( '/includes/front/seofunctions.php' );
+require_once get_theme_file_path( '/classes/class-twentynineteen-svg-icons.php' );
+require_once get_theme_file_path( '/includes/front/icon-functions.php' );
+require_once get_theme_file_path( '/includes/front/social-nav.php' );
 require_once get_theme_file_path( '/includes/front/preconnects.php' );
 require_once get_theme_file_path( '/includes/editor/blocks.php' );
 
@@ -32,10 +35,11 @@ add_action( 'wp_enqueue_scripts', 'wfd_enqueue_styles', 10 );
 add_action( 'after_setup_theme', 'wfd_editor_setup', 15 );
 add_action( 'after_setup_theme', 'wfd_editor_gradients', 15 );
 add_action( 'acf/init', 'wfd_services_block' );
+add_action( 'init', 'wfd_register_social_menu' );
+add_action( 'astra_footer_before', 'wfd_add_social_links' );
 add_filter( 'script_loader_tag', 'wfd_filter_script_loader_tag', 10, 2 );
 add_filter( 'script_loader_tag', 'wfd_defer_parsing_of_js', 11, 1 );
 add_filter( 'wp_resource_hints', 'wfd_resource_hints', 10, 2 );
 add_filter( 'astra_color_palettes', 'wfd_astra_color_palettes', 15 );
 add_filter( 'astra_single_post_navigation', 'wfd_change_next_prev_text', 10, 1 );
 add_filter( 'get_avatar', 'wfd_gravatar_alt' );
-
---
+++ includes/front/social-nav.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Wfd-child Social Navigation with SVG Icons.
+ *
+ * @package WordPress
+ * @subpackage Wfd-child
+ * @since wfd-child 1.0.0
+ */
+
+/**
+ * Register Social Navigation Menu.
+ */
+function wfd_register_social_menu() {
+	register_nav_menus(
+		array(
+			'social' => __( 'Social Links Menu', 'wfd' ),
+		)
+	);
+}
+
+function wfd_add_social_links() {
+	if ( has_nav_menu( 'social' ) ) {
+		?>
+		<nav class="poo-navigation" aria-label="<?php esc_attr_e( 'Social Links Menu', 'twentynineteen' ); ?>">
+		<?php
+
+		wp_nav_menu(
+			array(
+				'theme_location' => 'social',
+				'menu_class'     => 'social-links-menu',
+				'link_before'    => '<span class="screen-reader-text">',
+				'link_after'     => '</span>' . twentynineteen_get_social_icon_svg( 'link' ),
+				'depth'          => 1,
+			)
+		)
+		?>
+		</nav><!-- .social-navigation -->
+		<?php
+	}
+}
